2edd32c1ca8e3ae05cb0f7680e60fb02ba76b326
Briankyalo <Expertcarter7@gmail.com.>
2025-12-05 14:16:15 +0300

    I added a comment to the script.

--- arguments.sh
+++ arguments.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#This is a test script for arguments and I am also using it to learn about Git and Github
+
 lines=$(ls -lh $1 | wc -l)
 
 echo "You have $(($lines -1)) objects in the $1 directory."
